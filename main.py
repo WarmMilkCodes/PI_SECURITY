08f935f5e927f9b08b34290acee686115292644e
WarmMilkCodes <mrguybanks@gmail.com>
2023-02-24 13:03:31 -0600

    Add facial detection

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ display_height = 480
 # Set intial brightness level (between 0 and 1)
 brightness = 0.5
 
+# Load the Haar Cascade classifier for facial detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
 while True:
     # Read the current frame from the video stream
     ret, frame = cap.read()
@@ -20,12 +23,22 @@ while True:
     # Resize the frame to the selected size
     frame = cv2.resize(frame, (display_width, display_height))
 
+    # Convert the frame to grayscale for face detection
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detects faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
     # Add a text label showing ther current brightness level
     text = f'Brightness: {brightness:.1f}'
     cv2.putText(frame, text, (10,30), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
 
     # Show the current frame in a window
-    cv2.imshow('Live Video Feed', frame)
+    cv2.imshow('Cox Security Video Feed', frame)
 
     # Wait for a key press
     key = cv2.waitKey(1)
